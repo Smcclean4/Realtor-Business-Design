e2aec1053cd7f2da2ac6ea48986f8fc0bfddbd82
Sidney McClean <sidney.mcclean@outlook.com>
2019-07-17 17:15:15 -0700

    Made Loading page for site

--- project/js/script.js
+++ project/js/script.js
@@ -12,4 +12,28 @@ $(document).ready(function(){
             })
         }
     })
+
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+const backGround = document.querySelector('#background-image');
+
+function init() {
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+        
+        main.style.display = 'block';
+        
+        setTimeout(() => {
+            main.style.opacity = 1;
+            backGround.style.display = 'block';
+            backGround.style.opacity = 0.2;
+        }, 50);
+    
+    }, 4000);
+}
+
+init();
+
 });
+
